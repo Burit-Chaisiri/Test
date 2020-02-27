efff08186bbfd9332e5e971e6eeeb21534ebf197
Burit Chaisiri <koolkoop2543@gmail.com>
2020-02-27 09:42:54 +0700

    fix  functional_test

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -19,33 +19,40 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertIn('Calculator', self.browser.title)  
 
 
-        x = self.browser.find_element_by_id('x')
-        x.send_keys('70')
+        number1 = self.browser.find_element_by_id('id_x')
+        number1.send_keys('70')
 
-        y = self.browser.find_element_by_id('y')  
-        y.send_keys('7')    
+        number2 = self.browser.find_element_by_id('id_y')  
+        number2.send_keys('7')    
 
         add=self.browser.find_element_by_id('add')
         add.send_keys(Keys.ENTER)
+        time.sleep(5)
 
-
-
-        x.send_keys('70') # ทำการใส่ค่าใหม่เนื่องจากหลังกดคำนวณ เลขในช่อง x,yจะหายไป
-        y.send_keys('7')
+        number1 = self.browser.find_element_by_id('id_x')
+        number1.send_keys('70') # ทำการใส่ค่าใหม่เนื่องจากหลังกดคำนวณ เลขในช่อง x,yจะหายไป
+        number2 = self.browser.find_element_by_id('id_y')
+        number2.send_keys('7')
         sub=self.browser.find_element_by_id('sub')
         sub.send_keys(Keys.ENTER) 
+        time.sleep(5)
 
-        x.send_keys('70') # ทำการใส่ค่าใหม่เนื่องจากหลังกดคำนวณ เลขในช่อง x,yจะหายไป
-        y.send_keys('7')
+        number1 = self.browser.find_element_by_id('id_x')
+        number1.send_keys('70') # ทำการใส่ค่าใหม่เนื่องจากหลังกดคำนวณ เลขในช่อง x,yจะหายไป
+        number2 = self.browser.find_element_by_id('id_y')
+        number2.send_keys('7')
         mul=self.browser.find_element_by_id('mul')
         mul.send_keys(Keys.ENTER)
+        time.sleep(5)
 
-        x.send_keys('70') # ทำการใส่ค่าใหม่เนื่องจากหลังกดคำนวณ เลขในช่อง x,yจะหายไป
-        y.send_keys('7')
+        number1 = self.browser.find_element_by_id('id_x')
+        number1.send_keys('70') # ทำการใส่ค่าใหม่เนื่องจากหลังกดคำนวณ เลขในช่อง x,yจะหายไป
+        number2 = self.browser.find_element_by_id('id_y')
+        number2.send_keys('7')
         div=self.browser.find_element_by_id('div')
         div.send_keys(Keys.ENTER)
         
-        time.sleep(10)
+        time.sleep(5)
         self.fail('Finish the test!')  
 
 if __name__ == '__main__':  
